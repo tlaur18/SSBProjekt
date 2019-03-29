82243d44c1c0c87e11a4eea08c489675264b6657
Omvk97 <oliverk.inbox@gmail.com>
2019-03-29 18:27:28 +0100

    Start Project With gitIgnore
    Added a gitIgnore file in order to make sure all the local stuff will not be commited.
    Co-authored-by: maloe18 <maloe18@student.sdu.dk>
    Co-authored-by: micpe18 <micpe18@student.sdu.dk>
    Co-authored-by: tlaur18 <tlaur18@student.sdu.dk>
    Co-authored-by: mortj18 <mortj18@student.sdu.dk>
    Co-authored-by: nikru18 <nikru18@student.sdu.dk>

---
+++ src/ssb/FXMLDocumentController.java
@@ -0,0 +1,28 @@
+package ssb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author olive
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

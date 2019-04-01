0a3e5d2175281ed1898c7331274dfe15c9f92cd3
Oliver van Komen <olvan18@student.sdu.dk>
2019-04-01 18:08:25 +0200

    Added Document + TableView + other proof of concept

---
+++ src/ssb/domain_layer/Document.java
@@ -0,0 +1,69 @@
+package ssb.domain_layer;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Document {
+
+    public enum type {
+        SAGSÅBNING, UDREDNING, FAGLIGVURDERING, INDSTILLING, HANDLEPLAN, AFGØRELSE, BESTILLING, STATUSNOTAT, OPFØLGNING
+    }
+
+    private String documentName;
+    private final Resident associatedResident;
+    private final type type;
+    private Date editDate;
+    private Date creationDate;
+
+    public Document(Resident associatedResident, type type) {
+        this.associatedResident = associatedResident;
+        this.type = type;
+        generateDocumentName();
+        setEditDate();
+        setCreationDate();
+    }
+
+    private void generateDocumentName() {
+        this.documentName = associatedResident.getName().substring(0, 3) + type.toString().substring(0, 3) + " Test";
+    }
+
+    private void setCreationDate() {
+        this.creationDate = new Date(new Random().nextInt(100000000) + 1000000);
+    }
+
+    private void setEditDate() {
+        this.editDate = new Date();
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public String getAssociatedResident() {
+        return associatedResident.getName();
+    }
+
+    public type getType() {
+        return type;
+    }
+
+    public Date getEditDate() {
+        return editDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "documentName=" + documentName 
+            + "\nassociatedResident=" + associatedResident 
+            + "\ntype=" + type 
+            + "\neditDate=" + editDate 
+            + "\ncreationDate=" + creationDate;
+    }
+    
+    
+
+}
--- src/ssb/presentation_layer/controllers/FXMLDocumentController.java
+++ src/ssb/presentation_layer/controllers/FXMLDocumentController.java
@@ -6,14 +6,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import ssb.domain_layer.Document;
 import ssb.domain_layer.Employee;
 import ssb.domain_layer.Resident;
 
@@ -32,29 +39,45 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button sagerFaneBtn;
     @FXML
-    private Button sortNameBtn;
-    @FXML
-    private Button sortResidentBtn;
-    @FXML
-    private Button sortTypeBtn;
-    @FXML
-    private Button sortEditBtn;
-    @FXML
-    private Button sortCreationBtn;
-    @FXML
-    private ListView<?> documentsListView;
-    @FXML
     private TextField searchResidentTxtField;
     @FXML
     private TextField searchDocNameTxtField;
     @FXML
     private Button createVUMDocBtn;
+    @FXML
+    private TableView<Document> vumDocumentTableView;
+    private ObservableList<Document> observableDocuments;
     private final String NEW_BEBOER_CHOICE = "Ny beboer";
-    private final Employee employee = new Employee(Employee.job.SAGSBEHANDLER, new ArrayList<>(Arrays.asList(new Resident("Phillip"), new Resident("Thomas"))));
+    private final List<Resident> residents = new ArrayList<>(Arrays.asList(new Resident("Oliver"), new Resident("Nicolai"), new Resident("Michael")));
+    private final Employee employee = new Employee(Employee.job.SAGSBEHANDLER, residents);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // ArrayList med VUM-dokumenter
+        List<Document> documents = new ArrayList<>(Arrays.asList(new Document(residents.get(0), Document.type.SAGSÅBNING),
+            new Document(residents.get(1), Document.type.UDREDNING), new Document(residents.get(2), Document.type.BESTILLING)));
+
+        // ObservableList som opdateres hvis "documents" Arraylisten opdateres
+        observableDocuments = FXCollections.observableArrayList(documents);
+
+        // Forbinder tableView med observable list med dokumenterne
+        vumDocumentTableView.setItems(observableDocuments);
+        for (Object column : vumDocumentTableView.getColumns().toArray()) {
+            TableColumn<Document, ?> column1 = (TableColumn<Document, ?>) column;
+            column1.prefWidthProperty().bind(vumDocumentTableView.widthProperty().divide(5));
+        }
+    }
+
+    @FXML
+    public void openDocumentAction(MouseEvent event) {
+        Document selectedDocument = vumDocumentTableView.getSelectionModel().getSelectedItem();
+        if (selectedDocument != null && event.getClickCount() == 2) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("VUM-Dokument");
+            alert.setHeaderText("Se her! Jeg fandt et dokument :O *Insert pikachu face here*");
+            alert.setContentText(selectedDocument.toString());
+            alert.showAndWait();
+        }
     }
 
     @FXML

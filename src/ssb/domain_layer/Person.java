8f4161da34fa63183865a42a95441f3983c6276c
Thomas Steenfeldt Laursen <tlaur18@student.sdu.dk>
2019-04-02 13:40:27 +0200

    Indhold til Person-klassen

--- src/ssb/domain_layer/Person.java
+++ src/ssb/domain_layer/Person.java
@@ -2,4 +2,28 @@ package ssb.domain_layer;
 
 public class Person {
 
+    private String firstName;
+    private String lastName;
+    private String phoneNr;
+    private String cprNr;
+    private String ID;
+
+    public Person() {
+        this("N/A", "N/A", "N/A", "N/A");
+    }
+
+    public Person(String firstName, String lastName, String phoneNr, String cprNr) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNr = phoneNr;
+        this.cprNr = cprNr;
+        this.ID = generateID();
+    }
+    
+    /*
+    ID: First four letters of the Person's name + birthdate (without the year)
+    */
+    private String generateID() {
+        return (firstName + lastName).substring(0, 4) + cprNr.substring(0, 4);
+    }
 }

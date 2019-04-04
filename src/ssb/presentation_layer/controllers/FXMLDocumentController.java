b0bf0d785072c9bd190d19e0c5bcbea3b8e59ecf
Oliver van Komen <olvan18@student.sdu.dk>
2019-04-04 14:49:29 +0200

    Koncept visning af FXML-stage skift

--- src/ssb/presentation_layer/controllers/FXMLDocumentController.java
+++ src/ssb/presentation_layer/controllers/FXMLDocumentController.java
@@ -1,5 +1,7 @@
 package ssb.presentation_layer.controllers;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,7 +12,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -120,6 +124,15 @@ public class FXMLDocumentController implements Initializable {
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()) {
             System.out.println("Your choice: " + result.get());
+            try {
+                Stage stage = new Stage();
+                URL url = new File("src/ssb/presentation_layer/fxml_documents/test_view.fxml").toURL();
+                stage.setScene(new Scene(FXMLLoader.load(url)));
+                stage.setTitle("Oliver forsøger");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ src/ssb/presentation_layer/controllers/Test_viewController.java
@@ -0,0 +1,22 @@
+package ssb.presentation_layer.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author olive
+ */
+public class Test_viewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

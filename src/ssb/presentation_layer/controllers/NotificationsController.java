491f4366272bcdc3a32c6480740c83b1e6eedd91
Michael <micpe18@student.sdu.dk>
2019-05-07 15:46:20 +0200

    Notification css v1
    Dette er starten på css style til notificationerne

--- src/ssb/presentation_layer/controllers/NotificationsController.java
+++ src/ssb/presentation_layer/controllers/NotificationsController.java
@@ -10,13 +10,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import ssb.domain_layer.Department;
 import ssb.domain_layer.Employee.Employee;
 import ssb.domain_layer.InformationBridge;
@@ -71,11 +79,13 @@ public class NotificationsController implements Initializable {
     }
 
     private GridPane assembleNotification(String name, String message, String creationDate) {
+        String employeeFirstName = loggedInEmployee.getFirstName().substring(0, 1).toUpperCase() + loggedInEmployee.getFirstName().substring(1);
+        String employeeLastName = loggedInEmployee.getLastName().substring(0, 1).toUpperCase() + loggedInEmployee.getLastName().substring(1);
+        String employeeName = employeeFirstName + " " + employeeLastName;
         GridPane gridPane = new GridPane();
 
         gridPane.addColumn(1);
         gridPane.addRow(1);
-        gridPane.gridLinesVisibleProperty().set(true);
         gridPane.setPadding(new Insets(10, 10, 10, 10));
 
         ColumnConstraints column1 = new ColumnConstraints();
@@ -99,6 +109,21 @@ public class NotificationsController implements Initializable {
         dateLabel.setPadding(new Insets(5, 5, 5, 5));
         gridPane.add(dateLabel, 1, 0);
         GridPane.setHalignment(dateLabel, HPos.RIGHT);
+        
+        //css assembling
+
+        notificationVbox.getStyleClass().add("vbox-notification");
+        if (employeeName.equals(name)) {
+            gridPane.getStyleClass().add("notification-currentUser");
+            for (Node node : gridPane.getChildren()) {
+                if (node instanceof Label) {
+                    node.getStyleClass().add("notification-label");
+                }
+                messageLabel.setFont(new Font(14));
+            }
+        } else {
+            gridPane.getStyleClass().add("notification-OtherUsers");
+        }
 
         return gridPane;
     }

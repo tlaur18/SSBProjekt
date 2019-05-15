63bd1d6b8a26f81af468a69301ff7b6255dfab42
Thomas Steenfeldt Laursen <tlaur18@student.sdu.dk>
2019-05-15 21:48:49 +0200

    Advarsel ved indtastning af ugyldig CPR-nummer i søgefeltet

--- src/ssb/presentation_layer/controllers/nybeboerController.java
+++ src/ssb/presentation_layer/controllers/nybeboerController.java
@@ -47,11 +47,35 @@ public class nybeboerController implements Initializable {
     private TextField lastNameTxtF;
     @FXML
     private Label requiredFieldsLbl;
+    @FXML
+    private Label searchLabel;
+    @FXML
+    private Label invalidSearchLabel;
 
     private CPRRegisterManager cprRegisterManager = CPRRegisterManager.getInstance();
+    private boolean searchCPRisValid;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        searchTxtF.textProperty().addListener((observable, oldValue, newValue) -> {
+            //This checks whether the CPR nr is valid.
+            boolean isNumeric = true;
+            try {
+                Double.parseDouble(newValue);
+            } catch (NumberFormatException e) {
+                isNumeric = false;
+            }
+
+            if (newValue.length() != 10 || !isNumeric) {
+                searchLabel.setVisible(false);
+                invalidSearchLabel.setVisible(true);
+                searchCPRisValid = false;
+            } else {
+                searchLabel.setVisible(true);
+                invalidSearchLabel.setVisible(false);
+                searchCPRisValid = true;
+            }
+        });
     }
 
     @FXML
@@ -61,36 +85,37 @@ public class nybeboerController implements Initializable {
 
     @FXML
     private void saveBtnHandler(ActionEvent event) {
-        if (requiredBoxCheck()) {
-            Resident newRes = new Resident(firstNameTxtF.getText(), lastNameTxtF.getText(), phoneTxtF.getText(), cprTxtF.getText());
-            newRes.setCityName(cityTxtF.getText());
-            newRes.setPostCode(zipTxtF.getText());
-            newRes.setStreetName(streetTxtF.getText());
-            Home currentHome = InformationBridge.getInstance().getCurrentHome();
-            EmployeeManager employeeManager = EmployeeManager.getInstance();
-            employeeManager.addResidentToHome(currentHome.getId(), newRes);
-            InformationBridge.getInstance().getCurrentHome().addResident(newRes);
-
-            Stage stage = (Stage) saveButton.getScene().getWindow();
-            stage.close();
-            try {
-                URL controllerUrl = new File("src/ssb/presentation_layer/fxml_documents/tabs/sagerTab.fxml").toURL();
-                FXMLLoader loader = new FXMLLoader(controllerUrl);
-                loader.load();
-                SagerTabController sagerTabController = loader.getController();
-                sagerTabController.selectVUMDialog(newRes);
-            } catch (MalformedURLException ex) {
-                System.out.println("Invalid URL: " + ex.getMessage());
-            } catch (IOException ex) {
-                System.out.println("Could not load url: " + ex.getMessage());
-            }
-        } else {
+        if (!informationIsValid()) {
             requiredFieldsLbl.setVisible(true);
+            return;
         }
 
+        Resident newRes = new Resident(firstNameTxtF.getText(), lastNameTxtF.getText(), phoneTxtF.getText(), cprTxtF.getText());
+        newRes.setCityName(cityTxtF.getText());
+        newRes.setPostCode(zipTxtF.getText());
+        newRes.setStreetName(streetTxtF.getText());
+        Home currentHome = InformationBridge.getInstance().getCurrentHome();
+        EmployeeManager employeeManager = EmployeeManager.getInstance();
+        employeeManager.addResidentToHome(currentHome.getId(), newRes);
+        InformationBridge.getInstance().getCurrentHome().addResident(newRes);
+
+        Stage stage = (Stage) saveButton.getScene().getWindow();
+        stage.close();
+        try {
+            URL controllerUrl = new File("src/ssb/presentation_layer/fxml_documents/tabs/sagerTab.fxml").toURL();
+            FXMLLoader loader = new FXMLLoader(controllerUrl);
+            loader.load();
+            SagerTabController sagerTabController = loader.getController();
+            sagerTabController.selectVUMDialog(newRes);
+        } catch (MalformedURLException ex) {
+            System.out.println("Invalid URL: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Could not load url: " + ex.getMessage());
+        }
     }
 
-    private boolean requiredBoxCheck() {
+    private boolean informationIsValid() {
+        //TODO - Make this better
         if (cprTxtF.getText().length() >= 4 && firstNameTxtF.getText().length() >= 4
                 && lastNameTxtF.getText().length() >= 4 && !phoneTxtF.getText().isEmpty()
                 && !mailTxtF.getText().isEmpty()) {
@@ -102,9 +127,13 @@ public class nybeboerController implements Initializable {
 
     @FXML
     private void searchBtnHandler(ActionEvent event) {
+        if (!searchCPRisValid) {
+            return;
+        }
+        
         String cprToSearch = searchTxtF.getText();
         HashMap<String, String> personData = cprRegisterManager.searchCPRRegister(cprToSearch);
-        
+
         if (personData.isEmpty()) {
             Dialog d = new Alert(Alert.AlertType.INFORMATION);
             d.setTitle("Opret ny beboer");
